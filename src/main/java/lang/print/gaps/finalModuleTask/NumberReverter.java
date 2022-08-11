2d927f97bb0b9876b738413569bb534dd6ea855f
ksen192 <ermakovich.ks@gmail.com>
2022-08-11 16:36:14 +0300

    solution

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int res = 0;
+
+        while (number != 0) {
+            int i = number % 10;
+            res = res * 10 + i;
+            number /= 10;
+
+        }
+        System.out.println(res);
     }
 }
